5f95c157233dcbde54254378cc9518bc618ad05a
itsjjrodedition <jrb32002@gmail.com>
2024-07-26 18:46:57 -0400

    remove logging

--- events/VoiceStateUpdate.js
+++ events/VoiceStateUpdate.js
@@ -4,20 +4,14 @@ module.exports = {
     name: Events.VoiceStateUpdate,
     once: false,
     async execute(oldState, newState) {
-        console.log(oldState.channelId);
-        console.log(newState.channelId);
-
         if (oldState.channelId && oldState.channelId !== newState.channelId) {
-            console.log('Voice channel changed');
         }
 
         if (newState.channelId !== null && newState.channelId !== oldState.channelId) {
-            console.log('Voice channel joined');
             newState.client.channels.fetch(newState.channelId).then(channel => {
                 channel.send(`[<t:${Math.floor(Date.now() / 1000)}:R>] ${newState.member.displayName} joined the voice channel!`);
             });
         } else if(oldState.channelId !== null && oldState.channelId !== newState.channelId) {
-            console.log('Voice channel left');
             oldState.client.channels.fetch(oldState.channelId).then(channel => {
                 channel.send(`[<t:${Math.floor(Date.now() / 1000)}:R>] ${oldState.member.displayName} left the voice channel!`);
             });
