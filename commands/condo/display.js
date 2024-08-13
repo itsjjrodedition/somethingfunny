af8f1450bc29fa9ddc88a4090749429cf57d90d4
itsjjrodedition <jrb32002@gmail.com>
2024-08-12 22:37:54 -0400

    im him

--- commands/condo/display.js
+++ commands/condo/display.js
@@ -6,35 +6,66 @@ module.exports = {
         .setDescription("Display the status of something.")
         .addSubcommand(subcommand =>
             subcommand
-                .setName("washer_dryer")
-                .setDescription("Display the status of the washer and dryer.")
+                .setName("washer")
+                .setDescription("Display the status of the washer.")
+        )
+        .addSubcommand(subcommand => 
+            subcommand
+                .setName("dryer")
+                .setDescription("Display the status of the dryer.")
         ),
 
+
     async execute(interaction) {
-        if (interaction.options.getSubcommand() === "washer_dryer") {
+        if (interaction.options.getSubcommand() === "washer") {
             const embed = new EmbedBuilder()
                 .setColor("#0000FF")
                 .setTitle("Washer")
-        var onButton = new ButtonBuilder()
+        var washerOnButton = new ButtonBuilder()
             .setCustomId('washeron')
             .setLabel('On')
             .setEmoji('🟢')
             .setStyle(ButtonStyle.Success);
 
-        var offButton = new ButtonBuilder()
+        var washerOffButton = new ButtonBuilder()
             .setCustomId('washeroff')
             .setLabel('Off')
             .setEmoji('🔴')
             .setStyle(ButtonStyle.Danger);
        
         const row = new ActionRowBuilder()
-            .addComponents( onButton, offButton.setDisabled(true) );
-        const row2 = new ActionRowBuilder()
-            .addComponents( onButton.setDisabled(true), offButton );
+            .addComponents( washerOnButton, washerOffButton.setDisabled(true) );
+        // const row2 = new ActionRowBuilder()
+        //     .addComponents( washerOnButton.setDisabled(true), washerOffButton );
 
         await interaction.reply({ embeds: [embed], components: [row] });
 
-        }
+        } else if (interaction.options.getSubcommand() === "dryer") {
+            const embed = new EmbedBuilder()    
+                .setColor("#FF0000")
+                .setTitle("Dryer")
+
+            var dryerOnButton = new ButtonBuilder()
+                .setCustomId('dryeron') 
+                .setLabel('On')
+                .setEmoji('🟢')
+                .setStyle(ButtonStyle.Success);
+
+            var dryerOffButton = new ButtonBuilder()
+                .setCustomId('dryeroff')
+                .setLabel('Off')
+                .setEmoji('🔴')
+                .setStyle(ButtonStyle.Danger);
+
+            const row = new ActionRowBuilder()
+                .addComponents( dryerOnButton, dryerOffButton.setDisabled(true) );
+
+            // const row2 = new ActionRowBuilder() 
+            //     .addComponents( dryerOnButton.setDisabled(true), dryerOffButton );
+
+            await interaction.reply({ embeds: [embed], components: [row] });
         
-    },
+        }
+
+    }
 };
--- events/InteractionCreate.js
+++ events/InteractionCreate.js
@@ -1,20 +1,115 @@
-const { Events } = require('discord.js');
+const { Events, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+const editJsonFile = require("edit-json-file");
+let file = editJsonFile(`${__dirname}/config.json`);
 
 module.exports = {  
     name: Events.InteractionCreate,
     once: false,
     execute: async (interaction) => {
-        if (!interaction.isChatInputCommand()) return;
-        const command = interaction.client.commands.get(interaction.commandName);
-        if (!command) {
-            console.error(`No command matching ${interaction.commandName} was found.`);
-            return;
+        if (interaction.isChatInputCommand()){
+            const command = interaction.client.commands.get(interaction.commandName);
+            if (!command) {
+                console.error(`No command matching ${interaction.commandName} was found.`);
+                return;
+            }
+            try {
+                await command.execute(interaction);
+            } catch (error) {
+                console.error(error);
+                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            }
         }
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(error);
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        }
+
+        if (interaction.isButton()) {
+
+            file.read();
+            const washerEmbed = new EmbedBuilder()
+                .setColor("#0000FF")
+                .setTitle("Washer")
+                .addFields(
+                    { name: 'State', value: `${file.get('washerState') === 'on' ? 'On' : 'Off'}`, inline: true },
+                )
+            var washerOnButton = new ButtonBuilder()
+                .setCustomId('washeron')
+                .setLabel('On')
+                .setEmoji('🟢')
+                .setStyle(ButtonStyle.Success);
+    
+            var washerOffButton = new ButtonBuilder()
+                .setCustomId('washeroff')
+                .setLabel('Off')
+                .setEmoji('🔴')
+                .setStyle(ButtonStyle.Danger);
+
+            file.read();
+            const dryerEmbed = new EmbedBuilder()
+                .setColor("#FF0000")
+                .setTitle("Dryer")
+                .addFields(
+                    { name: 'State', value: `${file.get('dryerState') === 'on' ? 'On' : 'Off'}`, inline: true },
+                )
+            var dryerOnButton = new ButtonBuilder()
+                .setCustomId('dryeron')
+                .setLabel('On')
+                .setEmoji('🟢')
+                .setStyle(ButtonStyle.Success);
+    
+            var dryerOffButton = new ButtonBuilder()
+                .setCustomId('dryeroff')
+                .setLabel('Off')
+                .setEmoji('🔴')
+                .setStyle(ButtonStyle.Danger);
+
+             if (interaction.customId === 'washeron') {
+                 await interaction.deferUpdate()
+                 file.set('washerState', 'on');
+                 file.save();
+                 file.read();
+                 washerEmbed.setFields(
+                    { name: 'State', value: `${file.get('washerState') === 'on' ? 'On' : 'Off'}`, inline: true },
+                )
+                var message = interaction.channel.messages.cache.get(interaction.message.id) || await interaction.channel.messages.fetch(interaction.message.id)
+                const row = new ActionRowBuilder().addComponents( washerOnButton.setDisabled(true), washerOffButton.setDisabled(false) );
+                message.edit({ embeds: [washerEmbed], components: [row] })
+
+             } else if (interaction.customId === 'washeroff') {
+                 await interaction.deferUpdate()
+                 file.set('washerState', 'off');
+                 file.save();
+                 file.read();
+                 washerEmbed.setFields(
+                    { name: 'State', value: `${file.get('washerState') === 'on' ? 'On' : 'Off'}`, inline: true },
+                )
+                var message = interaction.channel.messages.cache.get(interaction.message.id) || await interaction.channel.messages.fetch(interaction.message.id)
+                const row = new ActionRowBuilder().addComponents( washerOnButton.setDisabled(false), washerOffButton.setDisabled(true) );
+                message.edit({ embeds: [washerEmbed], components: [row] })
+             }
+
+             if (interaction.customId === 'dryeron') {
+                await interaction.deferUpdate()
+                file.set('dryerState', 'on');
+                file.save();
+                file.read();
+               dryerEmbed.setFields(
+                   { name: 'State', value: `${file.get('dryerState') === 'on' ? 'On' : 'Off'}`, inline: true },
+               )
+               var message = interaction.channel.messages.cache.get(interaction.message.id) || await interaction.channel.messages.fetch(interaction.message.id)
+               const row = new ActionRowBuilder().addComponents( dryerOnButton.setDisabled(true), dryerOffButton.setDisabled(false) );
+               message.edit({ embeds: [dryerEmbed], components: [row] })
+
+            } else if (interaction.customId === 'dryeroff') {
+                await interaction.deferUpdate()
+                file.set('dryerState', 'off');
+                file.save();
+                file.read();
+                dryerEmbed.setFields(
+                    { name: 'State', value: `${file.get('dryerState') === 'on' ? 'On' : 'Off'}`, inline: true },
+                )
+               var message = interaction.channel.messages.cache.get(interaction.message.id) || await interaction.channel.messages.fetch(interaction.message.id)
+               const row = new ActionRowBuilder().addComponents( dryerOnButton.setDisabled(false), dryerOffButton.setDisabled(true) );
+               message.edit({ embeds: [dryerEmbed], components: [row] })
+            }
+         }
+
     }
 }
