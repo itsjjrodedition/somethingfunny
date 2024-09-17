634a5c81c3d0386ca38edd1da58eb650c62eac34
itsjjrodedition <jrb32002@gmail.com>
2024-09-16 20:53:08 -0400

    made it so if you join english or spanish it plays the song :)

--- events/VoiceStateUpdate.js
+++ events/VoiceStateUpdate.js
@@ -1,4 +1,5 @@
 const { Events } = require('discord.js');
+const { createAudioPlayer, NoSubscriberBehavior, createAudioResource, AudioPlayerStatus, joinVoiceChannel } = require('@discordjs/voice');
 
 module.exports = {
     name: Events.VoiceStateUpdate,
@@ -9,7 +10,41 @@ module.exports = {
 
         if (newState.channelId !== null && newState.channelId !== oldState.channelId) {
             newState.client.channels.fetch(newState.channelId).then(channel => {
+                if (channel.id != '624839576204345354') return
+                
                 // channel.send(`[<t:${Math.floor(Date.now() / 1000)}:R>] ${newState.member.user.globalName} joined the voice channel!`);
+                const connection = joinVoiceChannel({
+                    channelId: channel.id,
+                    guildId: channel.guild.id,
+                    adapterCreator: channel.guild.voiceAdapterCreator,
+                });
+
+                const player = createAudioPlayer({
+                    behaviors: {
+                        noSubscriber: NoSubscriberBehavior.Pause
+                    }
+                });
+                const resource = createAudioResource('./data/static.mp3');
+                player.play(resource);
+                connection.subscribe(player);
+                
+
+                player.on(AudioPlayerStatus.Playing, () => {
+                    console.log('The audio player has started playing!');
+                });
+                
+                player.on(AudioPlayerStatus.Idle, () => {
+                    console.log('The audio player has stopped playing!');
+                });
+
+                player.on(AudioPlayerStatus.Buffering, () => {
+                    console.log('The audio player is buffering!');
+                })
+
+                player.on('error', error => {
+                    console.error(`Error: ${error.message} with resource ${error.resource.metadata.title}`);
+                });
+
             });
         } else if(oldState.channelId !== null && oldState.channelId !== newState.channelId) {
             oldState.client.channels.fetch(oldState.channelId).then(channel => {
